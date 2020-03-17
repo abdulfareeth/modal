e8c91e404a0a76c2086d374242b57c7cd5d5358d
abdul fareeth <abdulfarina@gmail.com>
2020-03-17 19:40:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var modal = document.getElementById('simpleModal');
+
+var modalBtn = document.getElementById('modalBtn');
+
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click',openModal);
+
+closeBtn.addEventListener('click',closeModal);
+
+windowBtn.addEventListener('click',clickoutside);
+
+function openModal(){
+    modal.style.display='block';
+}
+
+function closeModal(){
+    modal.style.display='none';
+}
+
+function outsideClick(e){
+    if(e.target==modal){
+        modal.style.display='none'
+    }
+}
+   
+
